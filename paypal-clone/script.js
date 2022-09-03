4aec4f19fd8120522834bf2cde2c84c07ac6caea
lbhMichael <71274744+lbhMichael@users.noreply.github.com>
2022-09-03 08:44:33 +0100

    added files

---
+++ paypal-clone/script.js
@@ -0,0 +1,11 @@
+// Navigation
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
+
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener('mouseover', () => {
+       dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+       document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+    })
+})
+
+// End of Navigation
